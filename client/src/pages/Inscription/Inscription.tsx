a533b3bcfabb3c5ac51c1daeec4163b9c78e9484
Fabien <fafa.verhulst@gmail.com>
2025-01-09 16:45:26 +0100

    ModifInscription

--- client/src/pages/Inscription/Inscription.tsx
+++ client/src/pages/Inscription/Inscription.tsx
@@ -1,11 +1,44 @@
-import "./Inscription.css";
+import "./connexion.css";
 
-function Inscription() {
+function Connexion() {
   return (
-    <div className="Inscription">
-      <h1>Inscription</h1>
+    <div className="connecter">
+      <div className="logo">
+        <img src="../src/assets/images/Logo.png" alt="logoo" />
+      </div>
+
+      <h1>SE CONNECTER</h1>
+
+      <div className="avatar"></div>
+
+      <form className="case">
+        <input type="text" placeholder="Prénom" />
+        <input type="text" placeholder="Nom" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Mots de passe" />
+        <input type="text" placeholder="Adresse" />
+
+        <div className="newsletter">
+          <input type="checkbox" />
+          Acceptez-vous de recevoir nos offres personnalisé par email
+        </div>
+
+        <button type="submit">S'inscrire</button>
+      </form>
+
+      <nav className="navigation">
+        <a href="/nosvins">NOS VINS</a>
+        <a href="/degustations">DEGUSTATION</a>
+        <a href="/reservations">MES RESERVATIONS</a>
+        <a href="/quizz">QUIZ</a>
+        <a href="/seconnecter">SE CONNECTER</a>
+      </nav>
+
+      <footer>
+        <p>© 2024 Au Verre De Vin. L'alcool est dangereux pour la santé</p>
+      </footer>
     </div>
   );
 }
 
-export default Inscription;
+export default Connexion;
