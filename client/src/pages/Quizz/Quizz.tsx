a321d2fdd8958365b9ac2f77935cb4df44962281
LudovicAd95 <ludo.plougonven@gmail.com>
2025-01-16 11:37:29 +0100

    quizz debut

--- client/src/pages/Quizz/Quizz.tsx
+++ client/src/pages/Quizz/Quizz.tsx
@@ -1,9 +1,28 @@
 import "./quizz.css";
+import { useEffect, useState } from "react";
+
+interface Question {
+  question_id: number;
+  question_text: string;
+}
 
 function Quizz() {
+  const [question, setQuestion] = useState<Question[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3310/api/question")
+      .then((response) => response.json())
+      .then((data) => setQuestion(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <div className="Quizz">
-      <h1>Quizz</h1>
+    <div>
+      {question.map((questions: Question) => (
+        <div key={questions.question_id}>
+          <h3>{questions.question_text}</h3>
+        </div>
+      ))}
     </div>
   );
 }
