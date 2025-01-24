95a5312db82f853e1c50c16fe9297ec827b2716f
Malaurynn <malauryn@icloud.com>
2025-01-24 11:16:13 +0100

    routes BO

--- client/src/pages/BackOffice/ListEventsBO.tsx
+++ client/src/pages/BackOffice/ListEventsBO.tsx
@@ -1,4 +1,5 @@
 import "./ListEventsBO.css";
+import { Link } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import NavBar from "../../components/NavBar/NavBar";
 
@@ -6,6 +7,7 @@ function ListEventsBO() {
   return (
     <div className="ListEventsBO">
       <NavBar />
+      <Link to="/backoffice">BackOffice</Link>
       <h1>ListEventsBO</h1>
       <Footer />
     </div>
