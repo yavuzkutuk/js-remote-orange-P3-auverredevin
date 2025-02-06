067f01baf5ff74d33fbcfb2bd34cfffea33268d7
sgriscelli <simon.griscelli@wanadoo.fr>
2025-02-06 15:29:38 +0100

    Add NavBar component to IndexBackOffice and Welcome pages for consistent navigation

--- client/src/pages/BackOffice/IndexBackOffice.tsx
+++ client/src/pages/BackOffice/IndexBackOffice.tsx
@@ -6,72 +6,81 @@ import "./IndexBackOffice.css";
 
 function IndexBackOffice() {
   return (
-    <Container maxWidth="md" sx={{ textAlign: "center", mt: 4 }}>
+    <>
       <NavBar />
-      <Box sx={{ bgcolor: "whitesmoke", p: 3, borderRadius: 2, mt: 5 }}>
-        <Typography
-          variant="h4"
-          component="h1"
-          sx={{ mb: 2, fontWeight: "bold" }}
-        >
-          Index Back Office
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: 2,
-          }}
-        >
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "#9f0c00",
-              ":hover": { backgroundColor: "#dd1e0d" },
-            }}
-            component={Link}
-            to="/suggestionsBO"
+      <Container maxWidth="md" sx={{ textAlign: "center", mt: 4 }}>
+        <Box sx={{ bgcolor: "whitesmoke", p: 3, borderRadius: 2, mt: 5 }}>
+          <Typography
+            variant="h4"
+            component="h1"
+            sx={{ mb: 2, fontWeight: "bold" }}
           >
-            Liste des suggestions
-          </Button>
-          <Button
-            variant="contained"
+            Index Back Office
+          </Typography>
+          <Box
+            component="img"
+            className="degustation"
+            src="../src/assets/images/Backoffice.png"
+            alt="backoffice"
+            sx={{ width: "100%", maxWidth: "20.5rem" }}
+          />
+          <Box
             sx={{
-              backgroundColor: "#9f0c00",
-              ":hover": { backgroundColor: "#dd1e0d" },
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: 2,
             }}
-            component={Link}
-            to="/utilisateursBO"
           >
-            Liste des utilisateurs
-          </Button>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "#9f0c00",
-              ":hover": { backgroundColor: "#dd1e0d" },
-            }}
-            component={Link}
-            to="/vinsBO"
-          >
-            Liste des vins
-          </Button>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "#9f0c00",
-              ":hover": { backgroundColor: "#dd1e0d" },
-            }}
-            component={Link}
-            to="/evenementsBO"
-          >
-            Liste des évenements
-          </Button>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#9f0c00",
+                ":hover": { backgroundColor: "#dd1e0d" },
+              }}
+              component={Link}
+              to="/suggestionsBO"
+            >
+              Liste des suggestions
+            </Button>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#9f0c00",
+                ":hover": { backgroundColor: "#dd1e0d" },
+              }}
+              component={Link}
+              to="/utilisateursBO"
+            >
+              Liste des utilisateurs
+            </Button>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#9f0c00",
+                ":hover": { backgroundColor: "#dd1e0d" },
+              }}
+              component={Link}
+              to="/vinsBO"
+            >
+              Liste des vins
+            </Button>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#9f0c00",
+                ":hover": { backgroundColor: "#dd1e0d" },
+              }}
+              component={Link}
+              to="/evenementsBO"
+            >
+              Liste des évenements
+            </Button>
+          </Box>
         </Box>
-      </Box>
-      <Footer />
-    </Container>
+        <Footer />
+      </Container>
+    </>
   );
 }
 
